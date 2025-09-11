5952d53deaf27387f113249acda94a85742d12f1
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-11 22:46:50 +0200

    Add domElements object with query selectors for UI elements

---
+++ src/dom.js
@@ -0,0 +1,37 @@
+export const domElements = {
+  navigation: document.querySelector('.nav'),
+  overlay: document.querySelector('.overlay'),
+  //
+  dashboardBtn: document.querySelector('.dashboard-btn'),
+  booksBtn: document.querySelector('.books-btn'),
+  membersBtn: document.querySelector('.members-btn'),
+  //
+  main: document.querySelector('.main'),
+  //
+  dashboard: document.querySelector('.dashboard'),
+  membersNumber: document.querySelector('.members_number'),
+  booksNumber: document.querySelector('.books_number'),
+  dashboardSeeAllBooks: document.querySelector('.dashboard-see-all-books'),
+  dashboardSeeAllMembers: document.querySelector('.dashboard-see-all-members'),
+  //
+  books: document.querySelector('.books'),
+  addNewBookBtn: document.querySelector('.add-new-book_btn'),
+  booksList: document.querySelector('.books-list'),
+  bookID: document.querySelector('.book-id-p'),
+  bookTitle: document.querySelector('.book-title-p'),
+  bookAuthor: document.querySelector('.book-author-p'),
+  bookStatus: document.querySelector('.book-status-p'),
+  editBookBtn: document.querySelector('.edit-book'),
+  deleteBookBtn: document.querySelector('.delete-book'),
+  //
+  members: document.querySelector('.members'),
+  addNewMemberBtn: document.querySelector('.add-new-member_btn'),
+  membersList: document.querySelector('.members-list'),
+  membershipNo: document.querySelector('.membership-no-p'),
+  memberName: document.querySelector('.name-p'),
+  phone: document.querySelector('.phone-p'),
+  email: document.querySelector('.email-p'),
+  memberFine: document.querySelector('.fine-p'),
+  editMemberBtn: document.querySelector('.edit-member'),
+  deleteMemberBtn: document.querySelector('.delete-member'),
+};
