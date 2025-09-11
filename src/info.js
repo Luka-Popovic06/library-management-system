de5426602830946631eeb3edba7f8e928c8d238f
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-11 22:39:31 +0200

    Add initial data for books and members

---
+++ src/info.js
@@ -0,0 +1,92 @@
+export const books = [
+  {
+    bookId: 1001,
+    title: 'In Search of Lost Time',
+    author: 'Marcel Proust',
+    status: 'Available',
+  },
+  {
+    bookId: 1002,
+    title: 'Ulysses',
+    author: 'James Joyce',
+    status: 'Issued',
+  },
+  {
+    bookId: 1003,
+    title: 'Don Quixote',
+    author: 'Miguel de Cervantes',
+    status: 'Available',
+  },
+  {
+    bookId: 1004,
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    status: 'Issued',
+  },
+  {
+    bookId: 1005,
+    title: 'War and Peace',
+    author: 'Leo Tolstoy',
+    status: 'Available',
+  },
+  {
+    bookId: 1006,
+    title: 'Hamlet',
+    author: 'William Shakespeare',
+    status: 'Available',
+  },
+  {
+    bookId: 1007,
+    title: 'Crime and Punishment',
+    author: 'Fyodor Dostoyevsky',
+    status: 'Issued',
+  },
+  {
+    bookId: 1008,
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    status: 'Available',
+  },
+  {
+    bookId: 1009,
+    title: 'The Iliad',
+    author: 'Homer',
+    status: 'Available',
+  },
+  {
+    bookId: 1010,
+    title: 'Search ',
+    author: 'Stefan Proust',
+    status: 'Available',
+  },
+];
+export const members = [
+  {
+    membershipNo: 101,
+    name: 'John Doe',
+    phone: '132 - 465 - 8791',
+    email: 'john@doe.com',
+    fine: 0,
+  },
+  {
+    membershipNo: 102,
+    name: 'John Doe',
+    phone: '132 - 465 - 8791',
+    email: 'janedoe@mail.com',
+    fine: 100,
+  },
+  {
+    membershipNo: 103,
+    name: 'Anamika',
+    phone: '111 - 235 - 8891',
+    email: 'anamika@gmail.com',
+    fine: 40,
+  },
+  {
+    membershipNo: 104,
+    name: 'Joe Shmoe',
+    phone: '156 - 495 - 8972',
+    email: 'joe@sc.co',
+    fine: 75,
+  },
+];
