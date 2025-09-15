c1dbcc18fb6b4506a360281f7bf3d2ddb979aa90
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-15 20:36:37 +0200

    add loadDefaultBooks function to initialize and render default books

--- src/initialData.js
+++ src/initialData.js
@@ -2,3 +2,18 @@ import { domElements } from './dom.js';
 import { books, members } from './info.js';
 import { createBook, createMember } from './libraryService.js';
 import { Book, Member } from './libraryCreator.js';
+export function loadDefaultBooks(manager) {
+  if (manager.getBooks().length > 0) return;
+  const booksArray = books;
+  booksArray.forEach(function (book) {
+    const newBook = new Book(book.bookId, book.title, book.author, book.status);
+    manager.pushBook(newBook);
+    createBook(
+      newBook.getBookId(),
+      newBook.getID(),
+      newBook.getTitle(),
+      newBook.getAuthor(),
+      newBook.getStatus()
+    );
+  });
+}
