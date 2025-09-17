b366bc0ac68147185a72431a0f51f99c3771534e
Luka Popovic <lukapopovic0111@gmail.com>
2025-09-17 18:42:53 +0200

    Add event listeners to capture member form input values (membership no, name, phone, email, fine due)

--- src/input.js
+++ src/input.js
@@ -15,3 +15,20 @@ domElements.inputAddBookTitle.addEventListener('input', function (e) {
 domElements.radioInput.addEventListener('input', function (e) {
   radioInputValue = e.target.value;
 });
+export let membershipNoValue;
+export let memberNameValue;
+export let memberPhoneValue;
+export let memberEmailValue;
+export let memberFineDueValue;
+domElements.inputMembershipNo.addEventListener('input', function (e) {
+  membershipNoValue = e.target.value;
+});
+domElements.inputMembersName.addEventListener('input', function (e) {
+  memberNameValue = e.target.value;
+});
+domElements.inputMembersPhone.addEventListener('input', function (e) {
+  memberPhoneValue = e.target.value;
+});
+domElements.inputMembersFineDue.addEventListener('input', function (e) {
+  memberFineDueValue = e.target.value;
+});
